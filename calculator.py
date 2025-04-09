acdbd91a9d7e0dc2b92acf4116244cdb6929d1e1
gillandKaden <gillandkaden@ufl.edu>
2025-04-09 10:14:25 -0400

    Committing to main

--- calculator.py
+++ calculator.py
@@ -5,7 +5,30 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+import math
+
+def add(a, b): a + b
+
+def subtract(a, b): a - b
+
+def multiply(a, b): a * b
+
+def divide(a, b):
+    try:
+        if a == 0:
+            raise ZeroDivisionError
+        b / a   # raise ZeroDivisionError if a == 0
+    except ValueError:
+        print("Number cannot be zero")
+def logarithm(a, b):
+    try:
+        if b <= 0:
+            raise ValueError
+        math.log(a,b) # use math library + try/catch
+    except ValueError:
+        print("Base cannot be less than zero")
+def exponent(a, b):
+    a ** b       # use math library + try/catch
+
 
 
